329d47fb99936669b00bc08c5aa41cf13c8278a7
severrabaen <39644970+severrabaen@users.noreply.github.com>
2019-09-17 20:46:44 +0900

    Update dijkstra.cpp

--- GraphTheory/dijkstra.cpp
+++ GraphTheory/dijkstra.cpp
@@ -1,7 +1,6 @@
-const int INF=999999999;
 vector<long long>dij;//distance
 struct edge { int to, cost; };
-vector<vector<edge>>G;//各頂点iからの辺情報(距離,行き先の頂点)
+vector<vector<edge>>G;
 
 void dijkstra(int n) {
 	priority_queue<pair<long long, long long>, vector<pair<long long, long long>>, greater<pair<long long, long long>>>que;
