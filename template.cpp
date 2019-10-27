988529ab4e64b2eb9e9fc6de3925d0c4ef2293c3
severrabaen <39644970+severrabaen@users.noreply.github.com>
2019-10-27 15:13:08 +0900

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,5 +1,3 @@
-//«template«
-
 #include "bits/stdc++.h"
 using namespace std;
 #define Would
@@ -65,9 +63,6 @@ double add(double a, double b) {
 	if (abs(a + b) < EPS * (abs(a) + abs(b))) { return 0; }
 	return a + b;
 }
-
-
-//ªtemplateª
 
 int main() {
 
