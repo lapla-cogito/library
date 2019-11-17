1062395fdfc011bf6444a3133f45f218b9abcd69
severrabaen <39644970+severrabaen@users.noreply.github.com>
2019-11-17 20:18:49 +0900

    sizeをいちいち切り替えるのが時間の無駄なので定数で一括変更できるようにした

--- NumberTheory/nCk mod p.cpp
+++ NumberTheory/nCk mod p.cpp
@@ -1,16 +1,16 @@
 void COMinit() {
 	fac[0] = 1, fac[1] = 1, finv[0] = 1, finv[1] = 1, inv[1] = 1;
-	for (int i = 2; i < 100005; ++i) {
+	for (int i = 2; i < SIZE; ++i) {
 		fac[i] = fac[i - 1] * i % MOD;
-		inv[i] = MOD - inv[MOD%i] * (MOD / i) % MOD;
+		inv[i] = MOD - inv[MOD % i] * (MOD / i) % MOD;
 		finv[i] = finv[i - 1] * inv[i] % MOD;
 	}
 }
 
 //calc
 long long COM(long long n, long long k) {
-	if (n < k) return 0;
-	if (n < 0 || k < 0) return 0;
+	if (n < k) { return 0; }
+	if (n < 0 || k < 0) { return 0; }
 	return fac[n] * (finv[k] * finv[n - k] % MOD) % MOD;
 }
 
