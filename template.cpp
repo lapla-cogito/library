f744e90ceed3b204957ccb31ce489c0e60852ae0
severrabaen <39644970+severrabaen@users.noreply.github.com>
2020-03-02 15:45:30 +0900

    幾何に合わせた(と思う)

--- template.cpp
+++ template.cpp
@@ -5,20 +5,36 @@ using namespace std;
 #define all(n)  n.begin(),n.end()
 #define rall(n) n.rbegin(),n.rend()
 typedef long long ll;
+typedef double D;
+typedef complex<D> P; //point
+typedef pair<P, P> L; // line
+typedef vector<P> Q; //polygon
+#define X real() //real part
+#define Y imag() //imaginary part
+#define LE(n,m) ((n) < (m) + EPS)
+#define GE(n,m) ((n) + EPS > (m))
+#define EQ(n,m) (abs((n)-(m)) < EPS)
 const ll INF = 2147483647;
 const ll MOD = 1e9 + 7;
-const double EPS = 1e-10;
-const long double pi = acos((long double)-1);//3.1415926535897932384626433832795028...
+const D EPS = 1e-10;
+const D pi = acos(-1); //3.1415...
 const ll SIZE = 2000005;
 int dx[] = { 1,0,-1,0 }, dy[] = { 0,1,0,-1 }, alp[30];
 ll fac[SIZE], finv[SIZE], inv[SIZE];
 vector<ll>dij;
 struct edge { ll to, cost; };
-vector<vector<edge> >G;
+vector<vector<edge>> G;
 template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
 template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
 
-ll mod_pow(ll a, ll b) {
+namespace std {
+	bool operator < (const P& a, const P& b) {
+		return real(a) != real(b) ? real(a) < real(b) : imag(a) < imag(b);
+	}
+}
+
+template<typename T>
+auto mod_pow(T a, T b) {
 	ll res = 1, mul = a;
 	for (int i = 0; i < 31; ++i) {
 		if (b >> i & 1) {
@@ -61,16 +77,11 @@ void outp(vector<T>v) {
 	}
 }
 
-double add(double a, double b) {
+double add(D a, D b) {
 	if (abs(a + b) < EPS * (abs(a) + abs(b))) { return 0; }
 	return a + b;
 }
 
-double len(int a, int b, int c, int d) {
-	return (double)sqrt((a - c) * (a - c) + (b - d) * (b - d));
-}
-
-
-int main() {
+int main(){
 
 }
