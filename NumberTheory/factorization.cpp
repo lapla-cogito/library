ae0be1220ecb3f0639a79530b8b8f18ffc2b3701
severrabaen <39644970+severrabaen@users.noreply.github.com>
2021-03-22 18:37:26 +0900

    Update factorization.cpp

--- NumberTheory/factorization.cpp
+++ NumberTheory/factorization.cpp
@@ -1,8 +1,8 @@
-vector<long long> factori(long long n) {
-	vector<long long>res;
-	long long mon = 2;
+vector<ll> factori(ll n) {
+	vector<ll>res;
+	ll mon = 2;
 	while (n >= mon * mon) {
-		if (n%mon == 0) {
+		if (n % mon == 0) {
 			res.push_back(mon);
 			n /= mon;
 		}
